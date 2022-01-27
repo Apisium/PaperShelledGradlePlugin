068dd2fffb31fa704108c77c1fbd087e3b79e8a9
InitAuther97 <1602375324@qq.com>
2022-01-27 16:43:23 +0800

    Adapted 1.18

--- plugin-build/buildSrc/src/main/java/Dependencies.kt
+++ plugin-build/buildSrc/src/main/java/Dependencies.kt
@@ -1,5 +1,5 @@
 object BuildPluginsVersion {
-    const val KOTLIN = "1.5.21"
-    const val PLUGIN_PUBLISH = "0.14.0"
+    const val KOTLIN = "1.6.10"//1.5.29
+    const val PLUGIN_PUBLISH = "0.15.0"
     const val VERSIONS_PLUGIN = "0.38.0"
 }
--- plugin-build/plugin/src/main/java/cn/apisium/papershelled/gradle/Tasks.kt
+++ plugin-build/plugin/src/main/java/cn/apisium/papershelled/gradle/Tasks.kt
@@ -12,14 +12,17 @@ import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.options.Option
 import org.gradle.workers.WorkerExecutor
+import java.io.IOException
+import java.lang.Exception
+import java.lang.reflect.AccessibleObject
+import java.net.URL
 import java.net.URLClassLoader
 import java.nio.charset.StandardCharsets
 import java.nio.file.FileSystems
 import java.nio.file.Files
 import java.nio.file.Path
 import javax.inject.Inject
-import kotlin.io.path.ExperimentalPathApi
-import kotlin.io.path.name
+import kotlin.io.path.*
 
 private const val GROUP = "papershelled"
 
@@ -78,12 +81,114 @@ abstract class GenerateMappedJarTask : DefaultTask() {
     @get:Optional
     abstract val paperShelledJar: RegularFileProperty
 
+    @get:Input
+    @get:Option(option = "paperShelledLib", description = "The directory of the libraries released by paper")
+    @get:Optional
+    abstract val paperShelledLib: RegularFileProperty
+
     @ExperimentalPathApi
     @TaskAction
     fun run() {
         Files.createDirectories(project.layout.tmp)
         Files.createDirectories(project.layout.cache)
         val reobf = reobfFile.get().asFile.toPath()
+        try {
+            val path = URLClassLoader(arrayOf(jarFile.get().asFile.toURI().toURL())).use {
+                val clipClazz = it.loadClass("io.papermc.paperclip.Paperclip")
+                val ctxClazz = it.loadClass("io.papermc.paperclip.DownloadContext")
+                val repoDir = Path.of(System.getProperty("bundlerRepoDir", ""))
+                val patches = clipClazz.getDeclaredMethod("findPatches").run {
+                    isAccessible = true
+                    invoke(null) as Array<*>
+                }
+                val downloadContext = clipClazz.getDeclaredMethod("findDownloadContext").run {
+                    isAccessible = true
+                    invoke(null)
+                }
+                require(!(patches.isNotEmpty() && downloadContext == null)) {
+                    "patches.list file found without a corresponding original-url file"
+                }
+                val baseFile = if (downloadContext != null) {
+                    try {
+                        downloadContext::class.java.getDeclaredMethod("download", Path::class.java)
+                            .apply { isAccessible = true }.invoke(downloadContext, repoDir)
+                    } catch (e: IOException) {
+                        throw Exception("Failed to download original jar", e)
+                    }
+                    downloadContext::class.java.getDeclaredMethod("getOutputFile", Path::class.java)
+                        .apply { isAccessible = true }.invoke(downloadContext, repoDir) as Path
+                } else {
+                    null
+                }
+                clipClazz.declaredMethods.filter { it.name == "extractAndApplyPatches" }[0].run {
+                    isAccessible = true
+                    invoke(null, baseFile, patches, repoDir) as Map<String, Map<String, URL>>
+                }
+            }
+            val paperDict = path["versions"];
+            require(paperDict != null) {
+                "Server jar output directory was not found."
+            }
+            val paperJar = paperDict.values.elementAt(0).toURI().toPath()
+            if (Files.exists(reobf)) Files.delete(reobf)
+            val obcVersion = FileSystems.newFileSystem(paperJar, null as ClassLoader?).use { fs ->
+                val data =
+                    Files.readAllBytes(fs.getPath("/META-INF/mappings/reobf.tiny")).toString(StandardCharsets.UTF_8)
+                Files.write(
+                    reobf, data.replaceFirst(LEFT[1], LEFT[0]).replaceFirst(RIGHT[1], RIGHT[0])
+                        .toByteArray(StandardCharsets.UTF_8)
+                )
+                Files.list(fs.getPath("/org/bukkit/craftbukkit"))
+                    .map { it.fileName.name }
+                    .filter { it.startsWith("v") }
+                    .findFirst().get()
+            }
+            logger.lifecycle("Found org.bukkit.craftbukkit: $obcVersion")
+            Files.write(
+                project.layout.cache.resolve("mappingVersion.txt"),
+                obcVersion.toByteArray(StandardCharsets.UTF_8)
+            )
+            val temp = project.layout.tmp.resolve("temp.jar")
+            val remapper = TinyRemapper.newRemapper()
+                .withMappings(TinyUtils.createTinyMappingProvider(reobf as Path, RIGHT[0], LEFT[0]))
+                .ignoreConflicts(true)
+                .fixPackageAccess(true)
+                .rebuildSourceFilenames(true)
+                .renameInvalidLocals(true)
+                .threads(-1)
+                .build()
+            try {
+                OutputConsumerPath.Builder(temp).build().use {
+                    it.addNonClassFiles(paperJar, NonClassCopyMode.FIX_META_INF, remapper)
+                    remapper.readInputs(paperJar)
+                    remapper.apply(it)
+                }
+            } finally {
+                remapper.finish()
+            }
+            JarRelocator(
+                temp.toFile(), paperShelledJar.get().asFile, listOf(
+                    Relocation("org.bukkit.craftbukkit.$obcVersion", "org.bukkit.craftbukkit")
+                )
+            ).run()
+            temp.deleteExisting()
+            //others, come up to our output folder!
+            val allOther = path["libraries"]
+            require(allOther != null) {
+                "No libraries folder is found"
+            }
+            val libFolder = paperShelledLib.get().asFile.toPath()
+            if (!libFolder.exists()) {
+                libFolder.createDirectories()
+            }
+            allOther.values.map { it.toURI().toPath() }.forEach {
+                it.copyTo(libFolder.resolve(it.fileName))
+            }
+        } catch (unused: UnsupportedClassVersionError) { legacy(reobf)
+        } catch (it: NoSuchMethodException) { it.printStackTrace();legacy(reobf) }
+    }
+
+    private fun legacy(reobf: Path) {
         val path = URLClassLoader(arrayOf(jarFile.get().asFile.toURI().toURL())).use {
             val m = it.loadClass("io.papermc.paperclip.Paperclip").getDeclaredMethod("setupEnv")
             m.isAccessible = true
@@ -103,7 +208,7 @@ abstract class GenerateMappedJarTask : DefaultTask() {
         Files.write(project.layout.cache.resolve("mappingVersion.txt"), obcVersion.toByteArray(StandardCharsets.UTF_8))
         val temp = project.layout.tmp.resolve("temp.jar")
         val remapper = TinyRemapper.newRemapper()
-            .withMappings(TinyUtils.createTinyMappingProvider(reobf as Path, RIGHT[0], LEFT[0]))
+            .withMappings(TinyUtils.createTinyMappingProvider(reobf, RIGHT[0], LEFT[0]))
             .ignoreConflicts(true)
             .fixPackageAccess(true)
             .rebuildSourceFilenames(true)
