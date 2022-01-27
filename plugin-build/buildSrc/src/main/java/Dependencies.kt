ba0acfbe5e3b54fa408903656f27bd13868225c6
InitAuther97 <1602375324@qq.com>
2022-01-27 17:59:26 +0800

    Adapted 1.18

--- plugin-build/buildSrc/src/main/java/Dependencies.kt
+++ plugin-build/buildSrc/src/main/java/Dependencies.kt
@@ -1,5 +1,5 @@
 object BuildPluginsVersion {
     const val KOTLIN = "1.6.10"//1.5.29
     const val PLUGIN_PUBLISH = "0.15.0"
-    const val VERSIONS_PLUGIN = "0.38.0"
+    const val VERSIONS_PLUGIN = "0.39.0"
 }
