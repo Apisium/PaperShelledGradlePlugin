daafc680aa08c3133f27d39d4187066a184756e5
Shirasawa <764798966@qq.com>
2022-01-28 16:06:22 +0800

    Update version

--- plugin-build/buildSrc/src/main/java/Coordinates.kt
+++ plugin-build/buildSrc/src/main/java/Coordinates.kt
@@ -1,7 +1,7 @@
 object PluginCoordinates {
     const val ID = "cn.apisium.papershelled"
     const val GROUP = "cn.apisium.papershelled"
-    const val VERSION = "1.0.8"
+    const val VERSION = "1.1.0"
     const val IMPLEMENTATION_CLASS = "cn.apisium.papershelled.gradle.Plugin"
 }
 
